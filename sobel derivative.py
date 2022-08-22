048a722079f1cf5d9ed43b55a479260f38c28597
Ateş Taş <104631119+AtoQing@users.noreply.github.com>
2022-08-22 12:10:37 +0300

    Update sobel derivative.py

--- sobel derivative.py
+++ sobel derivative.py
@@ -4,7 +4,7 @@ from numpy import sqrt
 
 
 img = cv2.imread("image.png")
-img= cv2.resize(img, (501,501))
+img= cv2.resize(img, (500,500))
 img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
@@ -13,8 +13,8 @@ cv2.imshow("Orginal Image", img)
 img1 = np.zeros_like(img)       #Create matrix for new values
 
 
-for i in range (1,499,1):
-    for j in range (1,499,1):
+for i in range (1,498,1):
+    for j in range (1,498,1):
         Gy = (-1)*img[i-1,j-1]+ (-2)*img[i-1,j]+ (-1)*img[i-1,j+1]+  1*img[i+1,j-1]+ 2*img[i+1,j]+ 1*img[i+1,j+1]
         Gx = (-1)*img[i,j]+ (-2)*img[i,j-1]+ (-1)*img[i+1,j-1]+   1*img[i-1,j+1]+ 2*img[i,j+1]+ 1*img[i+1,j+1]
         G = sqrt(Gx**2+Gy**2)
